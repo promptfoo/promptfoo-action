268693a90e52bf013935890e6984ba5687b2d15f
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-03 15:44:43 -0700

    merge main

--- src/main.ts
+++ src/main.ts
@@ -58,8 +58,30 @@ export async function run(): Promise<void> {
       {required: false},
     );
 
-    core.setSecret(openaiApiKey);
+    const apiKeys = [
+      openaiApiKey,
+      azureApiKey,
+      anthropicApiKey,
+      huggingfaceApiKey,
+      awsAccessKeyId,
+      awsSecretAccessKey,
+      replicateApiKey,
+      palmApiKey,
+      vertexApiKey,
+    ];
+    for (const key of apiKeys) {
+      if (key) {
+        core.setSecret(key);
+      }
+    }
     core.setSecret(githubToken);
+
+    const event = github.context.eventName;
+    if (event !== 'pull_request') {
+      core.warning(
+        `This action is designed to run on pull request events only, but a "${event}" event was received.`,
+      );
+    }
 
     const pullRequest = github.context.payload.pull_request;
     if (!pullRequest) {
@@ -82,70 +104,81 @@ export async function run(): Promise<void> {
       headFetchHead,
     ]);
 
-    // Run promptfoo evaluation only for changed files
-    if (promptFiles.length > 0) {
-      const outputFile = path.join(process.cwd(), 'output.json');
-      let promptfooArgs = ['eval', '-c', configPath, '-o', outputFile];
-      if (!useConfigPrompts) {
-        // Resolve glob patterns to file paths
-        const promptFiles: string[] = [];
-        for (const globPattern of promptFilesGlobs) {
-          const matches = glob.sync(globPattern);
-          const changedMatches = matches.filter(
-            file => file !== configPath && changedFiles.includes(file),
-          );
-          promptFiles.push(...changedMatches);
-        }
-        promptfooArgs = promptfooArgs.concat(['--prompts', ...promptFiles]);
-      }
-      if (!noShare) {
-        promptfooArgs.push('--share');
-      }
+    // Resolve glob patterns to file paths
+    const promptFiles: string[] = [];
+    for (const globPattern of promptFilesGlobs) {
+      const matches = glob.sync(globPattern);
+      const changedMatches = matches.filter(
+        file => file !== configPath && changedFiles.includes(file),
+      );
+      promptFiles.push(...changedMatches);
+    }
 
-      const env = {
-        ...process.env,
-        ...(openaiApiKey ? {OPENAI_API_KEY: openaiApiKey} : {}),
-        ...(azureApiKey ? {AZURE_OPENAI_API_KEY: azureApiKey} : {}),
-        ...(anthropicApiKey ? {ANTHROPIC_API_KEY: anthropicApiKey} : {}),
-        ...(huggingfaceApiKey ? {HF_API_TOKEN: huggingfaceApiKey} : {}),
-        ...(awsAccessKeyId ? {AWS_ACCESS_KEY_ID: awsAccessKeyId} : {}),
-        ...(awsSecretAccessKey
-          ? {AWS_SECRET_ACCESS_KEY: awsSecretAccessKey}
-          : {}),
-        ...(replicateApiKey ? {REPLICATE_API_KEY: replicateApiKey} : {}),
-        ...(palmApiKey ? {PALM_API_KEY: palmApiKey} : {}),
-        ...(vertexApiKey ? {VERTEX_API_KEY: vertexApiKey} : {}),
-        ...(cachePath ? {PROMPTFOO_CACHE_PATH: cachePath} : {}),
-      };
+    const configChanged = changedFiles.includes(configPath);
+    if (promptFiles.length < 1 && !configChanged) {
+      // Run promptfoo evaluation only when files change.
+      core.info('No LLM prompt or config files were modified.');
+      return;
+    }
+
+    const outputFile = path.join(process.cwd(), 'output.json');
+    let promptfooArgs = ['eval', '-c', configPath, '-o', outputFile];
+    if (!useConfigPrompts) {
+      promptfooArgs = promptfooArgs.concat(['--prompts', ...promptFiles]);
+    }
+    if (!noShare) {
+      promptfooArgs.push('--share');
+    }
+
+    const env = {
+      ...process.env,
+      ...(openaiApiKey ? {OPENAI_API_KEY: openaiApiKey} : {}),
+      ...(azureApiKey ? {AZURE_OPENAI_API_KEY: azureApiKey} : {}),
+      ...(anthropicApiKey ? {ANTHROPIC_API_KEY: anthropicApiKey} : {}),
+      ...(huggingfaceApiKey ? {HF_API_TOKEN: huggingfaceApiKey} : {}),
+      ...(awsAccessKeyId ? {AWS_ACCESS_KEY_ID: awsAccessKeyId} : {}),
+      ...(awsSecretAccessKey
+        ? {AWS_SECRET_ACCESS_KEY: awsSecretAccessKey}
+        : {}),
+      ...(replicateApiKey ? {REPLICATE_API_KEY: replicateApiKey} : {}),
+      ...(palmApiKey ? {PALM_API_KEY: palmApiKey} : {}),
+      ...(vertexApiKey ? {VERTEX_API_KEY: vertexApiKey} : {}),
+      ...(cachePath ? {PROMPTFOO_CACHE_PATH: cachePath} : {}),
+    };
+    let errorToThrow: Error | undefined;
+    try {
       await exec.exec(`npx promptfoo@${version}`, promptfooArgs, {env});
+    } catch (error) {
+      // Ignore nonzero exit code, but save the error to throw later
+      errorToThrow = error as Error;
+    }
 
-      // Comment PR
-      const octokit = github.getOctokit(githubToken);
-      const output = JSON.parse(
-        fs.readFileSync(outputFile, 'utf8'),
-      ) as OutputFile;
-      const modifiedFiles = promptFiles.join(', ');
-      let body = `⚠️ LLM prompt was modified in these files: ${modifiedFiles}
+    // Comment PR
+    const octokit = github.getOctokit(githubToken);
+    const output = JSON.parse(
+      fs.readFileSync(outputFile, 'utf8'),
+    ) as OutputFile;
+    const modifiedFiles = promptFiles.join(', ');
+    let body = `⚠️ LLM prompt was modified in these files: ${modifiedFiles}
 
 | Success | Failure |
 |---------|---------|
 | ${output.results.stats.successes}      | ${output.results.stats.failures}       |
 
 `;
-      if (output.shareableUrl) {
-        body = body.concat(
-          `**» [View eval results](${output.shareableUrl}) «**`,
-        );
-      } else {
-        body = body.concat('**» View eval results in CI console «**');
-      }
-      await octokit.rest.issues.createComment({
-        ...github.context.repo,
-        issue_number: pullRequest.number,
-        body,
-      });
+    if (output.shareableUrl) {
+      body = body.concat(`**» [View eval results](${output.shareableUrl}) «**`);
     } else {
-      core.info('No LLM prompt files were modified.');
+      body = body.concat('**» View eval results in CI console «**');
+    }
+    await octokit.rest.issues.createComment({
+      ...github.context.repo,
+      issue_number: pullRequest.number,
+      body,
+    });
+
+    if (errorToThrow) {
+      throw errorToThrow;
     }
   } catch (error) {
     if (error instanceof Error) {
