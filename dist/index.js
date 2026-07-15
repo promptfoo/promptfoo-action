585e6940c414bc3154548851bb576864333ca90f
Michael D'Angelo <mdangelo@openai.com>
2026-07-14 18:15:34 -0700

    build: apply canonical Windows-prompt bundle

--- dist/index.js
+++ dist/index.js
@@ -29159,17 +29159,19 @@ function gte(i2, y2) {
 }
 function expand_(str, max, isTop) {
   const expansions = [];
-  const m = balanced("{", "}", str);
-  if (!m)
-    return [str];
-  const pre = m.pre;
-  const post = m.post.length ? expand_(m.post, max, false) : [""];
-  if (/\$$/.test(m.pre)) {
-    for (let k3 = 0; k3 < post.length && k3 < max; k3++) {
-      const expansion = pre + "{" + m.body + "}" + post[k3];
-      expansions.push(expansion);
+  for (; ; ) {
+    const m = balanced("{", "}", str);
+    if (!m)
+      return [str];
+    const pre = m.pre;
+    if (/\$$/.test(m.pre)) {
+      const post2 = m.post.length ? expand_(m.post, max, false) : [""];
+      for (let k3 = 0; k3 < post2.length && k3 < max; k3++) {
+        const expansion = pre + "{" + m.body + "}" + post2[k3];
+        expansions.push(expansion);
+      }
+      return expansions;
     }
-  } else {
     const isNumericSequence = /^-?\d+\.\.-?\d+(?:\.\.-?\d+)?$/.test(m.body);
     const isAlphaSequence = /^[a-zA-Z]\.\.[a-zA-Z](?:\.\.-?\d+)?$/.test(m.body);
     const isSequence = isNumericSequence || isAlphaSequence;
@@ -29177,10 +29179,12 @@ function expand_(str, max, isTop) {
     if (!isSequence && !isOptions) {
       if (m.post.match(/,(?!,).*\}/)) {
         str = m.pre + "{" + m.body + escClose + m.post;
-        return expand_(str, max, true);
+        isTop = true;
+        continue;
       }
       return [str];
     }
+    const post = m.post.length ? expand_(m.post, max, false) : [""];
     let n7;
     if (isSequence) {
       n7 = m.body.split(/\.\./);
@@ -29244,8 +29248,8 @@ function expand_(str, max, isTop) {
         }
       }
     }
+    return expansions;
   }
-  return expansions;
 }
 
 // node_modules/minimatch/dist/esm/assert-valid-pattern.js
